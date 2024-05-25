3be1ba1e3311c4025204163811702d8976d91017
kenchin627 <ken0110325@gmail.com>
2024-05-25 21:31:27 +0800

    UserLogin done half collection Done

---
+++ app/src/main/java/fcu/app/FengChiaFood/ShopCollection.java
@@ -0,0 +1,42 @@
+package fcu.app.FengChiaFood;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShopCollection extends AppCompatActivity {
+    private RecyclerView store_list;
+    private List<ShopDetails> shopDetailsList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_shop_collection);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        store_list = findViewById(R.id.StoreList);
+        store_list.setLayoutManager(new LinearLayoutManager(this));
+        shopDetailsList = new ArrayList<>();
+        shopDetailsList.add(new ShopDetails(R.drawable.menmonster, "Men Monster", "4.4"));
+        shopDetailsList.add(new ShopDetails(R.drawable.hun2, "HUN 貳", "4.5"));
+        shopDetailsList.add(new ShopDetails(R.drawable.dawang, "大王麻辣乾麵-逢甲旗艦店", "4.7"));
+        shopDetailsList.add(new ShopDetails(R.drawable.dapu, "大埔鐵板燒 河南逢甲店", "3.9"));
+        shopDetailsList.add(new ShopDetails(R.drawable.zhengxian, "爭鮮迴轉壽司-逢甲店", "4.0"));
+
+        StoreListAdapter adapter = new StoreListAdapter(this, shopDetailsList);
+        store_list.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/fcu/app/FengChiaFood/UserLogin.java
@@ -0,0 +1,62 @@
+package fcu.app.FengChiaFood;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UserLogin extends AppCompatActivity {
+
+    private EditText Email;
+    private EditText Password;
+    private Button login;
+    private String mail = "tom@fcu.edu.tw";
+    private String pass = "fcu123";
+    public static final String USERNAME ="User Name";
+    public static final String PASSWORD ="Password";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Email = findViewById(R.id.Email);
+        Password = findViewById(R.id.Password);
+        login = findViewById(R.id.btn_login);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = Email.getText().toString();
+                String password = Password.getText().toString();
+
+                if(username.equals(mail) && password.equals(pass)){
+                    Bundle bundle  = new Bundle();
+                    bundle.putString(USERNAME,username);
+                    bundle.putString(PASSWORD,password);
+
+                    Intent intent = new Intent();
+                    intent.setClass(UserLogin.this,MainActivity.class);
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(UserLogin.this,"賬號密碼錯誤，請重新輸入",Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+        login.setOnClickListener(listener);
+    }
+}
